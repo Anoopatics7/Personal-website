4633deb157e9209b456b3b40647ce5beae90ec9d
Anoop M Y <75351936+Anoopatics7@users.noreply.github.com>
2022-07-14 17:59:41 +0530

    icons
    images

--- script.js
+++ script.js
@@ -13,4 +13,41 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    
+    //typing animation
+    var typed = new Typed(".typing", {
+        strings: ["YouTuber", "Animal Person" , "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-1", {
+        strings: ["YouTuber", "Animal Person" , "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    //Owl carousel script
+    $('.carousel').owlCarousel({
+       margin: 20 ,
+       loop : true ,
+       autoplay:true,
+       autoplayTimeout:1500,
+       autoplayHoverPause:true,
+       responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false
+        }
+       }
+    });
 });
